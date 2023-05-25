884eadae69c24ae6cc4c5804db1a5d9d25541ff1
João Victor Magrão <joaovmagrao10@gmail.com>
2023-05-24 21:36:50 -0300

    Mascara no campo de valor e telefone

--- client/src/pages/Cliente.js
+++ client/src/pages/Cliente.js
@@ -1,4 +1,6 @@
 import Header from '../components/Header';
+import InputMask from 'react-input-mask';
+import { NumericFormat   } from 'react-number-format';
 
 function Cliente(){
 
@@ -16,7 +18,7 @@ function Cliente(){
               </div>
               <div>
                   <label htmlFor="telefoneAluno" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Telefone</label>
-                  <input type="tel" id="telefoneAluno" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required/>
+                  <InputMask  mask="(99) 99999-9999" type="tel" id="telefoneAluno" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required/>
               </div>
 
               <div className="mb-6">
@@ -26,7 +28,14 @@ function Cliente(){
           
               <div>
                   <label htmlFor="websmensalidadeAluno" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Valor</label>
-                  <input type="number" id="mensalidadeAluno" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                  <NumericFormat    thousandSeparator={true}
+          prefix={'R$'}
+          decimalScale={2}
+          fixedDecimalScale={true}
+          allowNegative={false}
+          allowLeadingZeros={false}
+          isNumericString={true}
+          getInputRef={inputRef => inputRef && inputRef.setAttribute('inputmode', 'numeric')} type="text" id="mensalidadeAluno" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
               </div>
             
               <div>
